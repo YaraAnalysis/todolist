2b0cf43795b8b336061daac70ab4a55f7348042e
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-10-14 14:31:11 -0300

    Ajusta a validação na rota de tasks
    Ajuste da validação de usuário logado, apenas para a rota de tasks.

--- src/main/java/br/com/yaravasconcellos/todolist/task/filter/FilterTaskAuth.java
+++ src/main/java/br/com/yaravasconcellos/todolist/task/filter/FilterTaskAuth.java
@@ -24,45 +24,49 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
-        // Pegar a autenticação (usuario e senha)
-        var authorization = request.getHeader("Authorization");
 
-        var authEncoded = authorization.substring("Basic".length()).trim();
+        var servletPath = request.getServletPath();
 
-        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+        if(servletPath.equals("/tasks/")) {
 
-        var authString = new String(authDecode);
-        
-        String[] credentials = authString.split(":");
-        String username = credentials[0];
-        String password = credentials[1];
-        System.out.println("Authorization");
-        System.out.println(username);
-        System.out.println(password);
+            // Pegar a autenticação (usuario e senha)
+            var authorization = request.getHeader("Authorization");
+
+            var authEncoded = authorization.substring("Basic".length()).trim();
+
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+            var authString = new String(authDecode);
+            
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+            System.out.println("Authorization");
+            System.out.println(username);
+            System.out.println(password);
 
 
-        // Validar usuário
-        var user = this.userRepository.findByUsername(username);
-        if(user == null) {
-            response.sendError(401);
-        }else {
-            // Validar a senha
-            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-            if(passwordVerify.verified) {
-                filterChain.doFilter(request, response);
-            }else {
+            // Validar usuário
+            var user = this.userRepository.findByUsername(username);
+            if(user == null) {
                 response.sendError(401);
-            }
-            // Segue viagem
+            }else {
+                // Validar a senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if(passwordVerify.verified) {
+                    filterChain.doFilter(request, response);
+                }else {
+                    response.sendError(401);
+                }
+                // Segue viagem
 
+            }
+        }else {
             filterChain.doFilter(request, response);
         }
 
     }
 
 }
-
-    
 
    
